1b181f58db7856097a8bc002689aca36cb8db5bd
Ophir Yael <ofir.yael@gmail.com>
2020-08-04 00:55:34 +0300

    Configure postgres db connection and app time zone

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-app',
+        'USER': 'harry',
+        'PASSWORD': 'potter',
+        'HOST': 'localhost',
+        'PORT': '5435',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Israel'
 
 USE_I18N = True
 
